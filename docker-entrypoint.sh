49113f2b0c55c18987c881270ecfdca6d022ca1d
Philipp Heuer <git@philippheuer.me>
2018-10-03 01:09:09 +0200

    feature: upgrade to v1.12.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ -n "$KUBECTL_CONFIG_CONTENT" ]; then
     # overwrite kubectl config file (useful for ci/cd)
     mkdir -p /root/.kube
-    echo $KUBECTL_CONFIG_CONTENT | base64 --decode >> /root/.kube/config
+    echo $KUBECTL_CONFIG_CONTENT | base64 --decode > /root/.kube/config
 fi
 
 exec "$@"
