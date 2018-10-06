a12c907ff61c33f5e7e2d82289c4984ec9ebff6d
Philipp Heuer <git@philippheuer.me>
2018-10-06 18:47:09 +0200

    refactor: rename KUBECTL_CONFIG_CONTENT to KUBECONFIG_CONTENT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ -n "$KUBECTL_CONFIG_CONTENT" ]; then
+if [ -n "$KUBECONFIG_CONTENT" ]; then
     # overwrite kubectl config file (useful for ci/cd)
     mkdir -p /root/.kube
-    echo $KUBECTL_CONFIG_CONTENT | base64 --decode > /root/.kube/config
+    echo $KUBECONFIG_CONTENT | base64 --decode > /root/.kube/config
 fi
 
 exec "$@"
